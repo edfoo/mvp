fd9d7f6791245cbfd725e262c560e310fcd1a963
Eduard Bergh <eduard@stonethree.com>
2025-06-15 23:10:39 +0200

    Update fail print

--- portfolio.py
+++ portfolio.py
@@ -35,7 +35,7 @@ def get_coin_avg(coin: dict):
         holding_value = avg * float(coin['holding'])
         return avg, cnt, holding_value
     else:
-        print(f"{slug}: failed to get data.")
+        print(f"{coin['name']}: failed to get data.")
         return None, None, None
 
 def main():
