50298ca3903902247c0217b285069227b893aa11
Eduard Bergh <eduard@stonethree.com>
2026-06-01 13:44:25 +0200

    Update example

--- sourceme.sh
+++ sourceme.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set-python 3.8
+
 python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 
 echo -e "Now you can run \n\npython portfolio.py -c example.json"
+
+python portfolio.py -c example.json
